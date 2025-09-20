2199461c96cd953f09f49795789eac85ccd920fa
Vlad <KhaGud@yandex.ru>
2025-09-20 20:55:54 +0300

    Feature: Added the ability to send POST requests

--- src/CoAP/CoAPClient.py
+++ src/CoAP/CoAPClient.py
@@ -1,6 +1,13 @@
+from coapthon.messages.request import Request
+from coapthon import defines
+
+import cbor2
+
 class CoAPClient:
-    def __init__(self, connector):
+    def __init__(self, connector, host, port):
         self.connector = connector
+        self.host = host
+        self.port = port
 
     def getServerStatus(self, pathToSource):
 
@@ -12,7 +19,14 @@ class CoAPClient:
             print(f"Erorr GET: {e}")
             return None
     
-    def sentSestData(self, pathToSource, testData):
-        pass
+    def sendPOSTRequest(self, pathToSource, rowData):
+            request = Request()
+            request.destination = (self.host, self.port)
+            request.code = defines.Codes.POST.number
+            request.uri_path = pathToSource
+            request.payload = rowData.hex()
+            request.content_type = defines.Content_types["application/cbor"]
+            self.connector.send_request(request)
+
     
 
--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from util.CoAPPropFileSearcher import serchCoAPProperties
 from CoAP.SyncCoAPConnector import SyncCoAPConnector
 from CoAP.CoAPClient import CoAPClient
 
+import cbor2
+
 
 def main():
 
@@ -15,8 +17,35 @@ def main():
     coapConnection = SyncCoAPConnector(confs.get("COAP_HOST"), int(confs.get("COAP_PORT")))
     coapConnection.connect()
 
-    client = CoAPClient(coapConnection.getConnect())
-    client.getServerStatus(confs.get("COAP_SOURCE"))
+    client = CoAPClient(coapConnection.getConnect(), confs.get("COAP_HOST"), int(confs.get("COAP_PORT")))
+
+    #------Temp Form---------------
+
+    # client.getServerStatus(confs.get("COAP_SOURCE"))
+
+    # deviceData = {    
+    #     "device_name": "ID_2F13433HFG12",       
+    #     "device_latitude": 55.755831, 
+    #     "device_longitude": 37.617673
+    # }
+
+    # testedData = {
+    #     "device_id":1,
+    #     "train_data_timestamp_input": [2025, 9, 12, 10, 52, 43],
+    #     "wheel_count_rail_input": 145,
+    #     "wheel_speed_rail_input": 1245,
+    #     "train_data_timestamp_output": [0, 0, 0, 0, 0, 0,],
+    #     "wheel_count_rail_output": 0,
+    #     "wheel_speed_rail_output": 0,
+    #     "common_Count_trains_entering_railway": 1,
+    #     "common_Count_train_wheels_entering_railway": 145
+    # }
+
+    # cbor_data = cbor2.dumps(testedData)
+
+    # decoded_data = cbor2.loads(cbor_data)
+
+    # client.sendPOSTRequest(confs.get("COAP_SOURCE"), cbor_data)
 
 
 
